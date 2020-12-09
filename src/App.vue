e3d8fea0c7d7ae2e99cde82d650dedd6615c8b9d
likailin <1399196696@qq.com>
2020-12-09 20:27:48 +0800

    舒适化项目-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,19 @@
     <div id="app">
         <router-view />
         <h1>vue根组件</h1>
+        <div>
+            <i class="toutiao toutiao_dianzan"></i>
+        </div>
+        <van-button type="primary">主要按钮</van-button>
+        <van-button type="info">信息按钮</van-button>
+        <van-button type="default">默认按钮</van-button>
+        <van-button type="warning">警告按钮</van-button>
+        <van-button type="danger">危险按钮</van-button>
+
+        <van-cell-group>
+            <van-cell title="单元格" value="内容" />
+            <van-cell title="单元格" value="内容" label="描述信息" />
+        </van-cell-group>
     </div>
 </template>
 
@@ -27,3 +40,7 @@
     }
 }
 </style>
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 加载Vant的核心组件
+import Vant from "vant";
+
+//加载的Vant全局样式
+import "vant/lib/index.css";
 
 // 导入全局样式
 import "./styles/index.less";
+
+// 注册Vant为组件
+Vue.use(Vant);
 
 Vue.config.productionTip = false;
 
